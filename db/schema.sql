9d7f4f254f3c500a51532dc5f9cd8afbcbc11085
Asher <asher.bergtraun@gmail.com>
2021-08-14 16:45:05 -0700

    added comments to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
+/*Identify if database already exists and replace it with a new empty one*/
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
+/* Change to new database */
 USE employees;
 
+/* Create a table for the organization's departments to be stored */
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
 
+/* Create a table for the organization's available roles to be stored */
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -14,6 +18,7 @@ CREATE TABLE role (
     department_id INTEGER
 );
 
+/* Create a table for the employees to be stored */
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
