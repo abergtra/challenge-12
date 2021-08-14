179ff8e79dc7e79a007712291ac1203df18b7503
Asher <asher.bergtraun@gmail.com>
2021-08-14 16:55:33 -0700

    syntax adjustment in databases

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE employees;
 
 /* Create a table for the organization's departments to be stored */
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 /* Create a table for the organization's available roles to be stored */
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INTEGER
@@ -20,7 +20,7 @@ CREATE TABLE role (
 
 /* Create a table for the employees to be stored */
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id DECIMAL(10, 2) NOT NULL,
