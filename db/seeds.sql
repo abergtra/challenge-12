3e34b01a51a09e2c206e09ddf7f57c122d46464e
Asher <asher.bergtraun@gmail.com>
2021-08-14 17:05:54 -0700

    added info to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
 INSERT INTO department(department_name)
 VALUES 
-    (''), 
-    (''), 
-    (''), 
-    (''), 
-    ('');
+    ('Sales'), 
+    ('Engineering'), 
+    ('Finance'), 
+    ('Legal'), 
+    ('Marketing');
 
 INSERT INTO role(title, salary, department_id)
 VALUES 
-    ('', , ), 
-    ('', , ), 
-    ('', , ), 
-    ('', , ), 
-    ('', , );
+    ('Software Engineer', , ), 
+    ('Mechanical Engineer', , ), 
+    ('Accountant', , ), 
+    ('Salesperson', , ), 
+    ('Lawyer', , );
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES 
-    ('', '', , ), 
-    ('', '', , ), 
-    ('', '', , ), 
-    ('', '', , ), 
-    ('', '', , );
+    ('Asher', 'Bergtraun', , ), 
+    ('Kelly', 'Manlaibayar', , ), 
+    ('Brian', 'Chen', , ), 
+    ('Adam', 'Chinn', , ), 
+    ('Keana', 'Smith', , );
